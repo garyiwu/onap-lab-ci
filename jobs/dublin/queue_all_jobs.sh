ff2e64cb711a4ba77a645ac4fe1843e23c0dd6d8
Gary Wu <gary.wu@gmail.com>
2019-06-19 16:56:23 -0700

    share queue_all_jobs.sh across jobs

--- jobs/dublin/queue_all_jobs.sh
+++ jobs/dublin/queue_all_jobs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-DIR=$(dirname "$(readlink -f "$0")")
+
+if [ $# -ne 1 ] || [ ! -f $1 ]; then
+    echo file not found
+    exit 1
+fi
+
+PODS_FILE=$1
+DIR=$(dirname "$(readlink -f "$PODS_FILE")")
 JOB=$(basename $DIR)
 echo $JOB
-for POD_TO_DELETE in $(cat $DIR/pods_to_delete.txt); do
+for POD_TO_DELETE in $(cat $PODS_FILE); do
     echo build "$JOB $POD_TO_DELETE"
     java -jar ~/jenkins-cli.jar  -s http://localhost:8080/jenkins -auth jenkins:g2jenkins build $JOB -p POD_TO_DELETE=$POD_TO_DELETE
 done
