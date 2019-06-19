4aad99b6a86391b417668880e92476233c3c7013
Gary Wu <gary.wu@gmail.com>
2019-06-19 15:14:11 -0700

    turn on job queuing

--- jobs/dublin/windriver-sb02-resilience-healthdist/queue_all_jobs.sh
+++ jobs/dublin/windriver-sb02-resilience-healthdist/queue_all_jobs.sh
@@ -4,5 +4,5 @@ JOB=$(basename $DIR)
 echo $JOB
 while read POD_TO_DELETE; do
     echo build "$JOB $POD_TO_DELETE"
-    #java -jar ~/jenkins-cli.jar  -s http://localhost:8080/jenkins -auth jenkins:g2jenkins build $JOB -p POD_TO_DELETE=$POD_TO_DELETE
+    java -jar ~/jenkins-cli.jar  -s http://localhost:8080/jenkins -auth jenkins:g2jenkins build $JOB -p POD_TO_DELETE=$POD_TO_DELETE
 done < $DIR/pods_to_delete.txt
