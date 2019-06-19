9bc6d87c1ce9607481776b6bbe48780eed2f1453
Gary Wu <gary.wu@gmail.com>
2019-06-19 16:23:07 -0700

    fix line iteration loop

--- jobs/dublin/windriver-sb02-resilience-healthdist/queue_all_jobs.sh
+++ jobs/dublin/windriver-sb02-resilience-healthdist/queue_all_jobs.sh
@@ -2,7 +2,7 @@
 DIR=$(dirname "$(readlink -f "$0")")
 JOB=$(basename $DIR)
 echo $JOB
-while read POD_TO_DELETE; do
+for POD_TO_DELETE in $(cat $DIR/pods_to_delete.txt); do
     echo build "$JOB $POD_TO_DELETE"
     java -jar ~/jenkins-cli.jar  -s http://localhost:8080/jenkins -auth jenkins:g2jenkins build $JOB -p POD_TO_DELETE=$POD_TO_DELETE
-done < $DIR/pods_to_delete.txt
+done
